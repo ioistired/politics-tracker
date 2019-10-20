31fb37ee93213626a6c0c4bd8328da2362d3f263
avery <averypeckstl@gmail.com>
2019-10-19 19:15:16 -0500

    Add first graphql query

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
 #!/usr/bin/env python3
 
 from quart import Quart
+import aiohttp
 
 app = Quart(__name__)
 
 @app.route('/')
 async def hello():
     return 'hello'
+
+@app.route('/bills')
+async def allbills():
+    response = run_query(query)
+    return response
+
+headers = {"X-API-KEY": "***REMOVED***"}
+
+
+def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://openstates.org/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+        
+# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
+query = """
+{
+  bills(jurisdiction: "Illinois", session: "101st", first: 20) {
+    edges {
+        node {
+            title
+            otherTitles {
+                title
+                note
+            }
+            subject
+            sources {
+              url
+            }
+            createdAt
+            updatedAt
+        }
+    }
+  }
+}
+
+"""
 
 if __name__ == '__main__':
 	app.run()
